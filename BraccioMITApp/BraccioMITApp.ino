518db552bc1f7c37942dacb3b0c543e329f4385f
Stefano Semeria <stefano.semeria@gmail.com>
2018-05-24 00:56:51 +0200

    Ultime versioni
    Ultime versioni

--- BraccioMITApp/BraccioMITApp.ino
+++ BraccioMITApp/BraccioMITApp.ino
@@ -6,6 +6,17 @@
 #define Bluetooth Serial1
 #define BUFFER_SIZE 256
 
+#define POSA1 0
+#define POSA2 1
+#define POSA3 2
+#define POSA4 3
+#define POSA5 4
+#define POSA6 5
+
+#define REALTIME_MODE 0
+#define PLAYBACK_MODE 1
+
+
 struct PosaBraccio 
 {
    int angoloServoBase;
@@ -13,11 +24,11 @@ struct PosaBraccio
    int angoloServoElbow;
    int angoloServoWristRot;
    int angoloServoWristVer;
-   int angoloServoGripper;
+   int angoloServoGripper ;
 
     PosaBraccio()
     {
-      
+      reset();
     }
 
     PosaBraccio(int m1,int m2, int m3, int m4, int m5, int m6)
@@ -53,6 +64,7 @@ struct PosaBraccio
 };
 
 
+
 Servo base;
 Servo shoulder;
 Servo elbow;
@@ -63,6 +75,90 @@ Servo gripper;
 PosaBraccio posaCorrente;
 
 PosaBraccio pose_salvate[6];
+
+int current_mode = REALTIME_MODE;
+int current_playback_pose = POSA1;
+bool  poseCrescenti = true;
+
+void posa_seguente() 
+{ 
+
+    Serial.print("Ultima posa numero ");
+    Serial.println(current_playback_pose);
+    if (poseCrescenti)
+    {
+          Serial.print("Incremento frame... valore corrente ");
+          Serial.println(current_playback_pose);
+          current_playback_pose++;
+          while (current_playback_pose < 6 && pose_salvate[current_playback_pose].IsInvalid()) 
+          {         
+              Serial.print("Salto valore ");
+              Serial.println(current_playback_pose);
+              current_playback_pose++;
+          }
+
+          //sono sono arrivato al limite: devo tornare indietro alla prima posizione buona e invertire il senso dei frame
+          if (current_playback_pose == 6)
+          {
+              
+                Serial.println("Sono al limite comincio a decrementare...");
+                poseCrescenti = false;
+    
+                current_playback_pose--;
+                Serial.print("Valore corrente ");
+                Serial.println(current_playback_pose);
+               //continua a decrementare saltando le pose non memorizzate
+                while (current_playback_pose >= 0 && pose_salvate[current_playback_pose].IsInvalid()) 
+                {
+                    Serial.print("Salto valore ");
+                    Serial.println(current_playback_pose);
+                    current_playback_pose--;
+                } 
+          }
+    }
+    else 
+    {
+          Serial.print("Decremento frame... valore corrente ");
+          Serial.println(current_playback_pose);
+          current_playback_pose--;
+          //continua a decrementare saltando le pose non memorizzate
+          while (current_playback_pose >= 0 && pose_salvate[current_playback_pose].IsInvalid()) 
+          {
+              Serial.print("Salto valore ");
+              Serial.println(current_playback_pose);
+              current_playback_pose--;
+          } 
+    
+           //sono sono arrivato al limite: devo tornare indietro alla prima posizione buona e invertire il senso dei frame
+          if (current_playback_pose < 0)
+          {
+              
+                Serial.println("Sono al limite comincio a incrementare...");
+                poseCrescenti = true;
+
+                current_playback_pose++;
+                Serial.print("Valore corrente ");
+                Serial.println(current_playback_pose);
+               //continua a decrementare saltando le pose non memorizzate
+                while (current_playback_pose < 6 && pose_salvate[current_playback_pose].IsInvalid()) 
+                {
+                    Serial.print("Salto valore ");
+                    Serial.println(current_playback_pose);
+                    current_playback_pose++;
+                }
+          }
+    }
+    
+    if (current_playback_pose < 0)
+      current_playback_pose = 0;
+     
+    if (current_playback_pose > 5)
+      current_playback_pose = 5;  
+
+    Serial.print("PROSSIMA POSA DA ASSUMERE: ");
+    Serial.println(current_playback_pose);
+    applica_posa(30, pose_salvate[current_playback_pose]);
+}
 
 
 void salva_posa(int numeroPosa, int y1, int y2, int y3, int y4, int y5, int y6) 
@@ -122,75 +218,96 @@ void loop()
       String str(buffer);
       Serial.println(str);  
 
-      //Se ricevo JSON, lo leggo e interpreto
-      if (str.indexOf('{') >= 0) 
-      {    
-        JsonObject& root = jsonBuffer.parseObject(buffer); 
-        if (root.success()) 
-        {
-            if (root.containsKey("m1")) 
-            {
-              int m1 = root["m1"];
-              posaCorrente.angoloServoBase = m1;
-            }
-            else if (root.containsKey("m2")) 
-            {
-              int m2 = root["m2"];
-              posaCorrente.angoloServoShoulder = m2;
-            }    
-            else if (root.containsKey("m3")) 
-            {
-              int m3 = root["m3"];
-              posaCorrente.angoloServoElbow = m3;
-            }
-            else if (root.containsKey("m4")) 
-            {
-              int m4 = root["m4"];
-              posaCorrente.angoloServoWristRot = m4;
-            }
-            else if (root.containsKey("m5")) 
-            {
-              int m5 = root["m5"];
-              posaCorrente.angoloServoWristVer = m5;
-            } 
-            else if (root.containsKey("m6")) 
-            {
-              int m6 = root["m6"];
-              posaCorrente.angoloServoGripper = m6;
-            }
-
-            applica_posa(30,posaCorrente); 
-        }
-        
-      } 
-      else if (str == "salva") 
+      if (str == "esegui") 
       {
-        
-      } 
-      else if (str == "esegui") 
-      {
-      
+             if (current_mode == REALTIME_MODE)
+              {
+                current_mode = PLAYBACK_MODE;
+                Serial.println("Modalita playback attivata");
+              }
+              else if (current_mode == PLAYBACK_MODE)
+              {  
+                current_mode = REALTIME_MODE;
+                Serial.println("Modalita realtime attivata");
+              }
       } 
       else if (str == "reset") 
       {
+         Serial.println("Reset delle posizioni!");
+         pose_salvate[0].reset();
+         pose_salvate[1].reset();
+         pose_salvate[2].reset();
+         pose_salvate[3].reset();
+         pose_salvate[4].reset();
+         pose_salvate[5].reset();
+         current_mode = REALTIME_MODE;
       }
-    }
+
+      if (current_mode == REALTIME_MODE)
+      {
+      
+          if (str.indexOf("s") == 0) 
+          {
+            int posizione = str.substring(1).toInt();
+            Serial.print("salvo Posizione ");
+            Serial.println(posizione);
+            salva_posa(posizione, posaCorrente.angoloServoBase, 
+                                  posaCorrente.angoloServoShoulder, 
+                                  posaCorrente.angoloServoElbow, 
+                                  posaCorrente.angoloServoWristRot, 
+                                  posaCorrente.angoloServoWristVer, 
+                                  posaCorrente.angoloServoGripper); 
+          }       
+          //Se ricevo JSON, lo leggo e interpreto
+          else if (str.indexOf('{') >= 0) 
+          {    
+            JsonObject& root = jsonBuffer.parseObject(buffer); 
+            if (root.success()) 
+            {
+                if (root.containsKey("m1")) 
+                {
+                  int m1 = root["m1"];
+                  posaCorrente.angoloServoBase = m1;
+                }
+                else if (root.containsKey("m2")) 
+                {
+                  int m2 = root["m2"];
+                  posaCorrente.angoloServoShoulder = m2;
+                }    
+                else if (root.containsKey("m3")) 
+                {
+                  int m3 = root["m3"];
+                  posaCorrente.angoloServoElbow = m3;
+                }
+                else if (root.containsKey("m4")) 
+                {
+                  int m4 = root["m4"];
+                  posaCorrente.angoloServoWristRot = m4;
+                }
+                else if (root.containsKey("m5")) 
+                {
+                  int m5 = root["m5"];
+                  posaCorrente.angoloServoWristVer = m5;
+                } 
+                else if (root.containsKey("m6")) 
+                {
+                  int m6 = root["m6"];
+                  posaCorrente.angoloServoGripper = m6;
+                }
+    
+            }
+            applica_posa(30,posaCorrente);
+          }     
+    } 
   }
+  }
+
+   if (current_mode == PLAYBACK_MODE) 
+   {   
+      //playback
+        posa_seguente();
+   }
  
-  delay(50);
-
- /* int Y1 = map(1, 0, 100, 0, 180);
-  int Y2 = map(2, 0, 100, 15, 165);
-  int Y3 = map(3, 0, 100, 0, 180);
-  int Y4 = map(4, 0, 100, 0, 180);
-  int Y5 = map(65, 0, 100, 0, 180);
-  int Y6 = map(7, 0, 100, 10, 73);*/
-
-
-    //  salva_posa(1,  Y1, Y2, Y3, Y4, Y5,  Y6);  
- 
-  
-   //
-//   Braccio.ServoMovement(30,         Y1, Y2, Y3, Y4, Y5,  Y6);  
+  delay(10); 
 
 }
--- BraccioXY/BraccioXY.ino
+++ BraccioXY/BraccioXY.ino
@@ -93,17 +93,17 @@ struct {
     // other variable
   uint8_t connect_flag;  // =1 if wire connected, else =0 
 
-} RemoteXY;
+} RemoteXY, LastRemoteXY;
 #pragma pack(pop)
 
 struct PosaBraccio 
 {
-   int angoloServoBase;
-   int angoloServoShoulder;
-   int angoloServoElbow;
-   int angoloServoWristRot;
-   int angoloServoWristVer;
-   int angoloServoGripper;
+   int angoloServoBase = -1;
+   int angoloServoShoulder = -1;
+   int angoloServoElbow = -1;
+   int angoloServoWristRot = -1;
+   int angoloServoWristVer = -1;
+   int angoloServoGripper = -1;
 
 
     void reset() 
@@ -144,6 +144,7 @@ Servo gripper;
 PosaBraccio pose_salvate[6];
 int current_mode = REALTIME_MODE;
 int current_playback_pose = POSA1;
+bool  poseCrescenti = true;
 
 void salva_posa(int numeroPosa, int y1, int y2, int y3, int y4, int y5, int y6) 
 {
@@ -160,47 +161,101 @@ void salva_posa(int numeroPosa, int y1, int y2, int y3, int y4, int y5, int y6)
 
 void posa_seguente() 
 { 
-    int last_pose = current_playback_pose;
+
+    Serial.print("Ultima posa numero ");
+    Serial.println(current_playback_pose);
+    if (poseCrescenti)
+    {
+          Serial.print("Incremento frame... valore corrente ");
+          Serial.println(current_playback_pose);
+          current_playback_pose++;
+          while (current_playback_pose < 6 && pose_salvate[current_playback_pose].IsInvalid()) 
+          {         
+              Serial.print("Salto valore ");
+              Serial.println(current_playback_pose);
+              current_playback_pose++;
+          }
+
+          //sono sono arrivato al limite: devo tornare indietro alla prima posizione buona e invertire il senso dei frame
+          if (current_playback_pose == 6)
+          {
+              
+                Serial.println("Sono al limite comincio a decrementare...");
+                poseCrescenti = false;
     
-    current_playback_pose++;
-    while (current_playback_pose < 6 && pose_salvate[current_playback_pose].IsInvalid()) 
-    {
-        current_playback_pose++;
-    } 
-
-    if (current_playback_pose < 6)
-    {
-      if (last_pose != current_playback_pose)
-      {
-         applica_posa(30, pose_salvate[current_playback_pose]);
-      }
-    }
-    else
-    {
-          current_playback_pose--;
-          while (current_playback_pose >= 0 && pose_salvate[current_playback_pose].IsInvalid()) 
-          {
-              current_playback_pose--;
-          } 
-
-          if (current_playback_pose >= 0) 
-          {
-            if (last_pose != current_playback_pose)
-            {
-              applica_posa(30, pose_salvate[current_playback_pose]); 
-            } 
+                current_playback_pose--;
+                Serial.print("Valore corrente ");
+                Serial.println(current_playback_pose);
+               //continua a decrementare saltando le pose non memorizzate
+                while (current_playback_pose >= 0 && pose_salvate[current_playback_pose].IsInvalid()) 
+                {
+                    Serial.print("Salto valore ");
+                    Serial.println(current_playback_pose);
+                    current_playback_pose--;
+                } 
           }
     }
+    else 
+    {
+          Serial.print("Decremento frame... valore corrente ");
+          Serial.println(current_playback_pose);
+          current_playback_pose--;
+          //continua a decrementare saltando le pose non memorizzate
+          while (current_playback_pose >= 0 && pose_salvate[current_playback_pose].IsInvalid()) 
+          {
+              Serial.print("Salto valore ");
+              Serial.println(current_playback_pose);
+              current_playback_pose--;
+          } 
+    
+           //sono sono arrivato al limite: devo tornare indietro alla prima posizione buona e invertire il senso dei frame
+          if (current_playback_pose < 0)
+          {
+              
+                Serial.println("Sono al limite comincio a incrementare...");
+                poseCrescenti = true;
 
+                current_playback_pose++;
+                Serial.print("Valore corrente ");
+                Serial.println(current_playback_pose);
+               //continua a decrementare saltando le pose non memorizzate
+                while (current_playback_pose < 6 && pose_salvate[current_playback_pose].IsInvalid()) 
+                {
+                    Serial.print("Salto valore ");
+                    Serial.println(current_playback_pose);
+                    current_playback_pose++;
+                }
+          }
+    }
+    
     if (current_playback_pose < 0)
       current_playback_pose = 0;
      
     if (current_playback_pose > 5)
       current_playback_pose = 5;  
+
+    Serial.print("PROSSIMA POSA DA ASSUMERE: ");
+    Serial.println(current_playback_pose);
+    applica_posa(30, pose_salvate[current_playback_pose]);
 }
 
 void applica_posa(int speed, PosaBraccio& posa) 
 {
+    
+    Serial.println("Playback mode... ");
+    Serial.print("Posizione (");
+    Serial.print(posa.angoloServoBase);
+    Serial.print(",");
+    Serial.print( posa.angoloServoShoulder);
+    Serial.print(",");
+    Serial.print(posa.angoloServoElbow);
+    Serial.print(",");
+    Serial.print(posa.angoloServoWristRot);
+    Serial.print(",");
+    Serial.print(posa.angoloServoWristVer);
+    Serial.print(",");
+    Serial.print(posa.angoloServoGripper);
+    Serial.print(")");
     Braccio.ServoMovement(speed,  posa.angoloServoBase, 
                                   posa.angoloServoShoulder, 
                                   posa.angoloServoElbow, 
@@ -211,14 +266,29 @@ void applica_posa(int speed, PosaBraccio& posa)
 
 void setup() 
 {
+    Serial.begin(9600);
     RemoteXY_Init (); 
     Braccio.begin();
-    Braccio.ServoMovement(30,         90, 90, 90, 90, 90,  10); 
+   // Braccio.ServoMovement(30,         90, 90, 90, 90, 90,  10); 
 }
 
 void loop() 
 { 
 
+  LastRemoteXY.P1 = RemoteXY.P1;
+  LastRemoteXY.P2 = RemoteXY.P2;
+  LastRemoteXY.P3 = RemoteXY.P3;
+  LastRemoteXY.P4 = RemoteXY.P4;
+  LastRemoteXY.P5 = RemoteXY.P5;
+  LastRemoteXY.P6 = RemoteXY.P6;
+  LastRemoteXY.play = RemoteXY.play;
+  LastRemoteXY.M2 = RemoteXY.M2;
+  LastRemoteXY.M3 = RemoteXY.M3;
+  LastRemoteXY.M4 = RemoteXY.M4;
+  LastRemoteXY.M5 = RemoteXY.M5;
+  LastRemoteXY.M6 = RemoteXY.M6;
+  LastRemoteXY.M1 = RemoteXY.M1;
+  LastRemoteXY.cancel = RemoteXY.cancel;
   
   RemoteXY_Handler ();
 
@@ -229,51 +299,76 @@ void loop()
   int Y5 = map(RemoteXY.M5, 0, 100, 0, 180);
   int Y6 = map(RemoteXY.M6, 0, 100, 10, 73);
 
- if (RemoteXY.P1 == 1) 
- {
-      salva_posa(POSA1, Y1, Y2, Y3, Y4, Y5, Y6);  
- } 
- else if (RemoteXY.P2 == 1) 
- {
-      salva_posa(POSA2, Y1, Y2, Y3, Y4, Y5, Y6);  
- }
- else if (RemoteXY.P3 == 1) 
- {
-      salva_posa(POSA3, Y1, Y2, Y3, Y4, Y5, Y6);  
- }
- else if (RemoteXY.P4 == 1) 
- {
-      salva_posa(POSA4, Y1, Y2, Y3, Y4, Y5, Y6);  
- }
- else if (RemoteXY.P5 == 1) 
- {
-      salva_posa(POSA5, Y1, Y2, Y3, Y4, Y5, Y6);  
- }
- else if (RemoteXY.P6 == 1) 
- {
-      salva_posa(POSA6, Y1, Y2, Y3, Y4, Y5, Y6);  
- } 
- else if (RemoteXY.play == 1) 
+ if (LastRemoteXY.play != RemoteXY.play && RemoteXY.play == 1) 
  {
       if (current_mode == REALTIME_MODE)
+      {
         current_mode = PLAYBACK_MODE;
+        Serial.println("Modalita playback attivata");
+      }
       else 
+      {  
         current_mode = REALTIME_MODE;
+        Serial.println("Modalita realtime attivata");
+      }
  } 
- else 
- {
-    if (current_mode == REALTIME_MODE) 
-    {
-        Braccio.ServoMovement(30, Y1, Y2, Y3, Y4, Y5,  Y6); 
-    } 
-    else 
-    {
-        posa_seguente();
-    }
- }
-    
+  else if (LastRemoteXY.cancel != RemoteXY.cancel && RemoteXY.cancel == 1) 
+  {
+     Serial.println("Reset delle posizioni!");
+     pose_salvate[0].reset();
+     pose_salvate[1].reset();
+     pose_salvate[2].reset();
+     pose_salvate[3].reset();
+     pose_salvate[4].reset();
+     pose_salvate[5].reset();
+     current_mode = REALTIME_MODE;
+  }
+
+      if (current_mode == REALTIME_MODE)
+      {
+      
+           if (LastRemoteXY.P1 != RemoteXY.P1 &&  RemoteXY.P1 == 1) 
+           {
+                Serial.println("Posa 1 memorizzata");
+                salva_posa(POSA1, Y1, Y2, Y3, Y4, Y5, Y6);  
+           } 
+           else if (LastRemoteXY.P2 != RemoteXY.P2 &&  RemoteXY.P2 == 1) 
+           {
+                Serial.println("Posa 2 memorizzata");
+                salva_posa(POSA2, Y1, Y2, Y3, Y4, Y5, Y6);  
+           }
+           else if (LastRemoteXY.P3 != RemoteXY.P3 && RemoteXY.P3 == 1) 
+           {
+                Serial.println("Posa 3 memorizzata");
+                salva_posa(POSA3, Y1, Y2, Y3, Y4, Y5, Y6);  
+           }
+           else if (LastRemoteXY.P4 != RemoteXY.P4 && RemoteXY.P4 == 1) 
+           {
+                Serial.println("Posa 4 memorizzata");
+                salva_posa(POSA4, Y1, Y2, Y3, Y4, Y5, Y6);  
+           }
+           else if (LastRemoteXY.P5 != RemoteXY.P5 && RemoteXY.P5 == 1) 
+           {
+                Serial.println("Posa 5 memorizzata");
+                salva_posa(POSA5, Y1, Y2, Y3, Y4, Y5, Y6);  
+           }
+           else if (LastRemoteXY.P6 != RemoteXY.P6 && RemoteXY.P6 == 1) 
+           {
+                Serial.println("Posa 6 memorizzata");
+                salva_posa(POSA6, Y1, Y2, Y3, Y4, Y5, Y6);  
+           }         
+           else 
+           {
+            
+                  Braccio.ServoMovement(30, Y1, Y2, Y3, Y4, Y5,  Y6); 
+            
+           }
+      }
+
+     if (current_mode == PLAYBACK_MODE) 
+     {   
+        //playback
+          posa_seguente();
+     }
  
-
-
-
 }
